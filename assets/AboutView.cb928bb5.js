2cf2ca8a1777a1c63db890d3f0a3a5ac4b7aa7df
LinDing <dinglin1991@gmail.com>
2022-10-08 23:32:14 +0800

    UI

--- assets/AboutView.cb928bb5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as _}from"./index.37bd33b7.js";const s={},c={class:"about"},n=_("h1",null,"\u672A\u5B8C\u5F85\u7EED...",-1),a=[n];function r(i,d){return t(),o("div",c,a)}const u=e(s,[["render",r]]);export{u as default};
